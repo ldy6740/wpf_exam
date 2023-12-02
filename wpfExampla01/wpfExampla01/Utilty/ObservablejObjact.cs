2d8ba36925ea197c25a5c8ef46e60b323b9e398f
ldy6740 <52596117+ldy6740@users.noreply.github.com>
2023-12-02 14:42:16 +0900

    example 01 upload

---
+++ wpfExampla01/wpfExampla01/Utilty/ObservablejObjact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfExampla01.Utilty
+{
+    public class ObservablejObjact : INotifyP
+    {
+
+    }
+}
